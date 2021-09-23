0c02f798bb81127035611712198a500fdea0c0c2
deriel <dennis.holmen@gmail.com>
2021-09-23 21:28:04 +0200

    förbättringar

--- UngeMozartdenjäveln.js
+++ UngeMozartdenjäveln.js
@@ -1,55 +1,70 @@
 "use strict";
+const [läggTillEventLyssnare, frågeVäljareAlla, bakgrundsPlatsX, bakgrundsPlatsY, bredd, höjd, sant, falskt, dokument, kropp, stil, fönster, begärAnimationsRuta, förVarje] = ["addEventListener", "querySelectorAll", "backgroundPositionX", "backgroundPositionY", "width", "height", true, false, document, document.body, "style", window, requestAnimationFrame, "forEach"]
 
-const target = { x: 0, y: 0 }
-const current = { x: 0, y: 0 }
-const max = { x: 0, y: 0 }
-const maxValue = { x: 0, y: 0 }
-const distanceOffset = maxDistance * 0.5
-const speed = 0.05
-const speedI = 1 - speed
-let raf = false
-let debounce = false
-const foo = document.body.style
-const animate = () => {
-    debounce = false
-    current.x = current.x * speedI + target.x * speed
-    if(current.x - target.x < 0.0001) { current.x = target.x }
-    current.y = current.y * speedI + target.y * speed
-    if(current.y - target.y < 0.0001) { current.y = target.y }
-    foo.backgroundPositionX = current.x + "px"
-    foo.backgroundPositionY = current.y + "px"
-    if (current.x == target.x && current.y == target.y) {
-        raf = false
+const mål = { x: 0, y: 0 }
+const sportNytt = { x: 0, y: 0 }
+const alltsåNånslagsGränsFårDetVälVa = { x: 0, y: 0 };
+const störstaVärdet = { x: 0, y: 0 }
+const hastighet = 0.05
+const inverteradHastighet = 1 - hastighet;
+let animeras = falskt
+let avstudsare = falskt
+const fä = kropp[stil]
+const rörPåDigDÅ = () => {
+    avstudsare = falskt
+    sportNytt.x = sportNytt.x * inverteradHastighet + mål.x * hastighet
+    if(sportNytt.x - mål.x < 0.0001) { sportNytt.x = mål.x };
+    sportNytt.y = sportNytt.y * inverteradHastighet + mål.y * hastighet
+    if(sportNytt.y - mål.y < 0.0001) { sportNytt.y = mål.y }
+    fä[bakgrundsPlatsX] = sportNytt.x + "px";
+    fä[bakgrundsPlatsY] = sportNytt.y + "px"
+    if (sportNytt.x == mål.x && sportNytt.y == mål.y) {
+        animeras = falskt
     } else {
-        requestAnimationFrame(animate)
+        begärAnimationsRuta(rörPåDigDÅ)
     }
 }
-const onresize = () => {
+const påomskalning = () => {
     const rect = document.body.getBoundingClientRect()
-    max.x = rect.width
-    max.y = rect.height
-    if(rect.width > rect.height) {
-        maxValue.x = maxDistance * (rect.height/rect.width)
-        maxValue.y = maxDistance
+    alltsåNånslagsGränsFårDetVälVa.x = rect[bredd];
+    alltsåNånslagsGränsFårDetVälVa.y = rect[höjd]
+    if(rect[bredd] > rect[höjd]) {
+        störstaVärdet.x = störstaDistansen * (rect[höjd]/rect[bredd]);
+        störstaVärdet.y = störstaDistansen
     } else {
-        maxValue.x = maxDistance
-        maxValue.y = maxDistance * (rect.width/rect.height) 
+        störstaVärdet.x = störstaDistansen;
+        störstaVärdet.y = störstaDistansen * (rect[bredd]/rect[höjd]) 
 
     }
-    console.log("resize", max)
 }
-onresize()
-window.addEventListener("resize", onresize)
-const para = (e) => {
-    if (!debounce) {
-        debounce = true
-        target.x = (e.x / max.x) * maxValue.x - maxValue.x
-        target.y = (e.y / max.y) * maxValue.y - maxValue.y
-        if (!raf) {
-            raf = true
-            requestAnimationFrame(animate)
+påomskalning()
+fönster[läggTillEventLyssnare]("resize", påomskalning)
+const lyssnare = (e) => {
+    if (!avstudsare) {
+        avstudsare = sant
+        mål.x = (e.x / alltsåNånslagsGränsFårDetVälVa.x) * störstaVärdet.x - störstaVärdet.x
+        mål.y = (e.y / alltsåNånslagsGränsFårDetVälVa.y) * störstaVärdet.y - störstaVärdet.y;
+        if (!animeras) {
+            animeras = sant
+            begärAnimationsRuta(rörPåDigDÅ)
         }
-        console.log(e.x, e.y)
-    }
+    };
 }
-document.body.addEventListener("mousemove", para)
+kropp[läggTillEventLyssnare]("mousemove", lyssnare)
+
+const klämtill = (x, liten, stor) => Math.min(stor, Math.max(liten, x));
+const shuffle = (matris) => [...matris].sort(() => Math.random()*2-1)
+const interpolera = (x, början, slutet) => (1-x) * början + x * slutet;
+const mjukare = (x, liten, stor, utjämnare) => interpolera(utjämnare(x), liten, stor)
+
+dokument[frågeVäljareAlla](".stjärna")[förVarje]((lillbabs) => {
+    let x = 0;
+    let riktning = shuffle(["left", "right", "top", "bottom"])[0]
+    let steg = mjukare(Math.random(), 1, 10, (x) => x*x*x);
+    let sovstund = mjukare(Math.random(), 5000, 25000, a => a*a)
+    const stjärnstoff = () => {
+        lillbabs.style[riktning] = (x -= Math.round(Math.random()*steg+1)) + "px"; 
+        setTimeout(stjärnstoff, Math.random()*sovstund+250);
+    }
+    setTimeout(stjärnstoff, Math.random()*10000+5000)
+})
