b9de1d3125f673a95c62b06eb3d36372dfb4e31c
deriel <dennis.holmen@gmail.com>
2021-09-25 00:56:46 +0200

    mer vargar

--- UngeMozartdenjäveln.js
+++ UngeMozartdenjäveln.js
@@ -1,15 +1,28 @@
-const [läggTillEventLyssnare, frågeVäljareAlla, bakgrundsPlatsX, bakgrundsPlatsY, bredd, höjd, sant, falskt, dokument, kropp, stil, fönster, begärAnimationsRuta, förVarje, hämtaGränsKlientFyrkant, omskalning, pekarförflyttning, matte] = ["addEventListener", "querySelectorAll", "backgroundPositionX", "backgroundPositionY", "width", "height", true, false, document, document.body, "style", window, requestAnimationFrame, "forEach", "getBoundingClientRect", "resize", "mousemove", Math]
+[läggTillHändelseSpanare, frågeVäljareAlla, bakgrundsPlatsX, bakgrundsPlatsY, bredd, höjd, sant, falskt, dokument, kropp, stil, fönster, begärAnimationsRuta, förVarje, hämtaGränsKlientFyrkant, omskalning, pekarförflyttning, matte, sättVäntetid] = ["addEventListener", "querySelectorAll", "backgroundPositionX", "backgroundPositionY", "width", "height", true, false, document, document.body, "style", window, requestAnimationFrame, "forEach", "getBoundingClientRect", "resize", "mousemove", Math, setTimeout]
 
-const mål = { x: 0, y: 0 }
-const sportNytt = { x: 0, y: 0 }
-const alltsåNånslagsGränsFårDetVälVa = { x: 0, y: 0 };
-const störstaVärdet = { x: 0, y: 0 }
-const hastighet = 0.05
-const inverteradHastighet = 1 - hastighet;
-let animeras = falskt
-let avstudsare = falskt
-const fä = kropp[stil]
-const rörPåDigDÅ = () => {
+
+
+
+klämtill = (x, liten, stor) => matte.min(stor, matte.max(liten, x));
+korsfalna = (x, a, b) => x*a + (1-x)*b;
+bjällerKurva = (x) => 1 - korsfalna(x, x*x, (1-x)*(1-x))
+interpolera = (x, början, slutet) => (1-x) * början + x * slutet;
+mjukare = (x, liten, stor, utjämnare) => interpolera(utjämnare(x), liten, stor)
+slumpa = (början, slutet, utjämnare = x => x) => mjukare(matte.random(), början, slutet, utjämnare)
+blanda = (matris) => [...matris].sort(() => slumpa(-1,1))
+
+
+
+mål = { x: 0, y: 0 }
+sportNytt = { x: 0, y: 0 }
+alltsåNånslagsGränsFårDetVälVa = { x: 0, y: 0 };
+störstaVärdet = { x: 0, y: 0 }
+hastighet = 0.05
+inverteradHastighet = 1 - hastighet;
+animeras = falskt
+avstudsare = falskt
+fä = kropp[stil]
+rörPåDigDÅ = () => {
     avstudsare = falskt
     sportNytt.x = sportNytt.x * inverteradHastighet + mål.x * hastighet
     if(sportNytt.x - mål.x < 0.0001) { sportNytt.x = mål.x };
@@ -17,14 +30,18 @@ const rörPåDigDÅ = () => {
     if(sportNytt.y - mål.y < 0.0001) { sportNytt.y = mål.y }
     fä[bakgrundsPlatsX] = sportNytt.x + "px";
     fä[bakgrundsPlatsY] = sportNytt.y + "px"
+    extraPara[förVarje](([djur, [karaktärsdragX, karaktärsdragY]]) => {
+        djur[stil][karaktärsdragX[0]] = sportNytt.x * karaktärsdragX[1] + "px";
+        djur[stil][karaktärsdragY[0]] = sportNytt.y * karaktärsdragY[1] + "px";
+    })
     if (sportNytt.x == mål.x && sportNytt.y == mål.y) {
         animeras = falskt
     } else {
         begärAnimationsRuta(rörPåDigDÅ)
     }
 }
-const påomskalning = () => {
-    const fyrkanten = kropp[hämtaGränsKlientFyrkant]()
+påomskalning = () => {
+    fyrkanten = kropp[hämtaGränsKlientFyrkant]()
     alltsåNånslagsGränsFårDetVälVa.x = fyrkanten[bredd];
     alltsåNånslagsGränsFårDetVälVa.y = fyrkanten[höjd]
     if(fyrkanten[bredd] > fyrkanten[höjd]) {
@@ -37,8 +54,10 @@ const påomskalning = () => {
     }
 }
 påomskalning()
-fönster[läggTillEventLyssnare](omskalning, påomskalning)
-kropp[läggTillEventLyssnare](pekarförflyttning, (e) => {
+fönster[läggTillHändelseSpanare](omskalning, påomskalning)
+
+sättVäntetid(() => {
+    kropp[läggTillHändelseSpanare](pekarförflyttning, (e) => {
     if (!avstudsare) {
         avstudsare = sant
         mål.x = (e.x / alltsåNånslagsGränsFårDetVälVa.x) * störstaVärdet.x - störstaVärdet.x
@@ -49,21 +68,14 @@ kropp[läggTillEventLyssnare](pekarförflyttning, (e) => {
         }
     };
 })
-
-klämtill = (x, liten, stor) => matte.min(stor, matte.max(liten, x));
-korsfalna = (x, a, b) => x*a + (1-x)*b;
-bjällerKurva = (x) => 1 - korsfalna(x, x*x, (1-x)*(1-x))
-interpolera = (x, början, slutet) => (1-x) * början + x * slutet;
-mjukare = (x, liten, stor, utjämnare) => interpolera(utjämnare(x), liten, stor)
-slumpa = (början, slutet, utjämnare = x => x) => mjukare(matte.random(), början, slutet, utjämnare)
-blanda = (matris) => [...matris].sort(() => slumpa(-1,1))
+}, avvakta || 0); 
 
 dokument[frågeVäljareAlla](".stjärna")[förVarje]((lillbabs) => {
-    let x = 0;
-    let riktning = blanda(["left", "right", "top", "bottom"])[0]
-    let steg = slumpa(1, 10, (x) => x*x*x);
-    let sovstund = slumpa(5000, 25000, a => a*a)
-    const stjärnstoff = () => {
+    x = 0;
+    riktning = blanda(["left", "right", "top", "bottom"])[0]
+    steg = slumpa(1, 10, (x) => x*x*x);
+    sovstund = slumpa(5000, 25000, a => a*a)
+    stjärnstoff = () => {
         lillbabs.style[riktning] = (x -= matte.round(matte.random()*steg+1)) + "px"; 
         setTimeout(stjärnstoff, matte.random()*sovstund+250);
     }
