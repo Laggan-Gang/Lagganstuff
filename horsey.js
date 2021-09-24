9378f4af0e21b6c90e0f62e24de828b02128561c
deriel <dennis.holmen@gmail.com>
2021-09-24 21:00:53 +0200

    hästarna pratar mer nu

--- horsey.js
+++ horsey.js
@@ -1,4 +1,4 @@
 (() => {
     ljud=blanda(document.getElementById("hästisar").children)
-    document.getElementById("horsey").addEventListener("mouseenter", (e) => { blanda(ljud);ljud[0].play();ljud[1].play() });
+    document.getElementById("horsey").addEventListener("mouseenter", (e) => { nyaljud = blanda(ljud);nyaljud[0].play();nyaljud[1].play() });
 })()
