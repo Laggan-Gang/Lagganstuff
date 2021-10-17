55f541ba35da68a2b3839039d86f56560e9769aa
deriel <dennis.holmen@gmail.com>
2021-10-17 23:49:46 +0200

    SARA

--- UngeMozartdenjäveln.js
+++ UngeMozartdenjäveln.js
@@ -1,4 +1,4 @@
-[läggTillHändelseSpanare, frågeVäljareAlla, bakgrundsPlatsX, bakgrundsPlatsY, bredd, höjd, sant, falskt, dokument, kropp, stil, fönster, begärAnimationsRuta, förVarje, hämtaGränsKlientFyrkant, omskalning, pekarförflyttning, Matte, sättVäntetid, sortera, slump, funktion, typav, om, avrunda, animering, ingen, absolut, notera, Matris] = ["addEventListener", "querySelectorAll", "backgroundPositionX", "backgroundPositionY", "width", "height", true, false, document, document.body, "style", window, requestAnimationFrame, "forEach", "getBoundingClientRect", "resize", "mousemove", Math, setTimeout, "sort", () => Matte.random(), "function", (variabel) => typeof variabel, (kanske, ja, nej = () => undefined) => (typeof kanske === funktion ? kanske() : kanske) ? ja() : nej(), (x) => Math.round(x), "animation", "none", (x) => Math.abs(x), (...data) => console.log(...data), Array]
+[läggTillHändelseSpanare, frågeVäljareAlla, bakgrundsPlatsX, bakgrundsPlatsY, bredd, höjd, sant, falskt, dokument, kropp, stil, fönster, begärAnimationsRuta, förVarje, hämtaGränsKlientFyrkant, omskalning, pekarförflyttning, Matte, sättVäntetid, sortera, slump, funktion, typav, om, avrunda, animering, ingen, absolut, notera, Matris, klicka, muterad, hämtaRadiatorEfterLegitimation, förminska, knuffa, skär, längd, barn, källa, ersätt, sökVäg, förläggning, klyv, sammanfoga, förena] = ["addEventListener", "querySelectorAll", "backgroundPositionX", "backgroundPositionY", "width", "height", true, false, document, "body", "style", window, requestAnimationFrame, "forEach", "getBoundingClientRect", "resize", "mousemove", Math, setTimeout, "sort", () => Matte.random(), "function", (variabel) => typeof variabel, (kanske, ja, nej = () => undefined) => (typeof kanske === funktion ? kanske() : kanske) ? ja() : nej(), (x) => Math.round(x), "animation", "none", (x) => Math.abs(x), (...data) => console.log(...data), Array, "click", "muted", i => document.getElementById(i), "reduce", "push", "slice", "length", "children", "src", "replace", "pathname", "location", "split", "concat", "join"]
 
 
 
@@ -9,7 +9,18 @@ bjällerKurva = (x) => 1 - korsfalna(x, x * x, (1 - x) * (1 - x))
 interpolera = (x, början, slutet) => (1 - x) * början + x * slutet;
 mjukare = (x, liten, stor, utjämnare) => interpolera(utjämnare(x), liten, stor)
 slumpa = (början, slutet, utjämnare = x => x) => mjukare(slump(), början, slutet, utjämnare)
-blanda = (matris) => [...matris][sortera](() => slumpa(-1, 1))
+blanda = (iMatris) => {
+    [blyack, matris] = [[], [...iMatris]]
+    matris[förVarje](() => {
+        blyack[knuffa](matris[skär](slumpa(0, matris[längd])))
+    })
+    return matris
+}
+
+sköterDuDig = slumpa(0, 9010)
+om(() => sköterDuDig > 9000, () => {
+    fönster[förläggning][sökVäg] = fönster[förläggning][sökVäg][klyv]("/")[skär](0, -1)[sammanfoga](["/skärpDig.html"])[förena]("/")
+})
 
 
 mål = { x: 0, y: 0 }
@@ -20,36 +31,36 @@ hastighet = 0.01
 inverteradHastighet = 1 - hastighet;
 animeras = falskt
 avstudsare = falskt;
-fä = kropp[stil]
-rörPåDigDÅ = () => {
-    avstudsare = falskt
-    sportNytt.x = sportNytt.x * inverteradHastighet + mål.x * hastighet
-    om(absolut(sportNytt.x - mål.x) < 0.0001, () => { sportNytt.x = mål.x });
-    sportNytt.y = sportNytt.y * inverteradHastighet + mål.y * hastighet
-    om(absolut(sportNytt.y - mål.y) < 0.0001, () => { sportNytt.y = mål.y })
-    fä[bakgrundsPlatsX] = sportNytt.x + "px";
-    fä[bakgrundsPlatsY] = sportNytt.y + "px"
-    om(fönster.extraPara, () => fönster.extraPara(störstaVärdet, sportNytt))
-    om(sportNytt.x == mål.x && sportNytt.y == mål.y, () => { animeras = falskt }, () => begärAnimationsRuta(rörPåDigDÅ, 250))
-}
-minStörstaDistans = window.störstaDistansen || 500;
-påomskalning = () => {
-    fyrkanten = kropp[hämtaGränsKlientFyrkant]()
-    alltsåNånslagsGränsFårDetVälVa.x = fyrkanten[bredd];
-    alltsåNånslagsGränsFårDetVälVa.y = fyrkanten[höjd]
-    om(fyrkanten[bredd] > fyrkanten[höjd], () => {
-        störstaVärdet.x = minStörstaDistans * (fyrkanten[höjd] / fyrkanten[bredd]);
-        störstaVärdet.y = minStörstaDistans
-    }, () => {
-        störstaVärdet.x = minStörstaDistans;
-        störstaVärdet.y = minStörstaDistans * (fyrkanten[bredd] / fyrkanten[höjd])
-    })
-}
-påomskalning()
-fönster[läggTillHändelseSpanare](omskalning, påomskalning)
-
 sättVäntetid(() => {
-    kropp[läggTillHändelseSpanare](pekarförflyttning, (händelse) => {
+    fä = dokument[kropp][stil]
+    rörPåDigDÅ = () => {
+        avstudsare = falskt
+        sportNytt.x = sportNytt.x * inverteradHastighet + mål.x * hastighet
+        om(absolut(sportNytt.x - mål.x) < 0.0001, () => { sportNytt.x = mål.x });
+        sportNytt.y = sportNytt.y * inverteradHastighet + mål.y * hastighet
+        om(absolut(sportNytt.y - mål.y) < 0.0001, () => { sportNytt.y = mål.y })
+        fä[bakgrundsPlatsX] = sportNytt.x + "px";
+        fä[bakgrundsPlatsY] = sportNytt.y + "px"
+        om(fönster.extraPara, () => fönster.extraPara(störstaVärdet, sportNytt))
+        om(sportNytt.x == mål.x && sportNytt.y == mål.y, () => { animeras = falskt }, () => begärAnimationsRuta(rörPåDigDÅ, 250))
+    }
+    minStörstaDistans = window.störstaDistansen || 500;
+    påomskalning = () => {
+        fyrkanten = dokument[kropp][hämtaGränsKlientFyrkant]()
+        alltsåNånslagsGränsFårDetVälVa.x = fyrkanten[bredd];
+        alltsåNånslagsGränsFårDetVälVa.y = fyrkanten[höjd]
+        om(fyrkanten[bredd] > fyrkanten[höjd], () => {
+            störstaVärdet.x = minStörstaDistans * (fyrkanten[höjd] / fyrkanten[bredd]);
+            störstaVärdet.y = minStörstaDistans
+        }, () => {
+            störstaVärdet.x = minStörstaDistans;
+            störstaVärdet.y = minStörstaDistans * (fyrkanten[bredd] / fyrkanten[höjd])
+        })
+    }
+    påomskalning()
+    fönster[läggTillHändelseSpanare](omskalning, påomskalning)
+
+    dokument[kropp][läggTillHändelseSpanare](pekarförflyttning, (händelse) => {
         om(() => !avstudsare, () => {
             avstudsare = sant
             mål.x = (händelse.x / alltsåNånslagsGränsFårDetVälVa.x) * störstaVärdet.x - störstaVärdet.x
